93e6e19884f05ba8788f1e02737d810741e1b324
Sonny Vagn Andersen <20115032@post.au.dk>
2022-02-14 13:46:04 +0100

    React project

---
+++ react-folio/src/AllCoins.tsx
@@ -0,0 +1,48 @@
+import React, { Component, useState, useEffect } from "react";
+
+export function AllCoins() {
+    const url: string = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2C%20ethereum%2C%20tether&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+
+    // const [btcImage, setBtcImage] = useState(null);
+    // const [name, setName] = useState(null);
+    // const [price, setPrice] = useState(null);
+
+    const [allCoinInfo, setAllCoinInfo] = useState<any[]>([]);
+
+    useEffect(() => {
+        fetch(url)
+        .then(resp => resp.json())
+        .then(data => {
+            // setBtcImage(data[0].image);
+            // setName(data[0].name);
+            // setPrice(data[0].current_price);
+            setAllCoinInfo(data);
+            });
+        const interval = setInterval(() => {
+            fetch(url)
+            .then(resp => resp.json())
+            .then(data => setAllCoinInfo(data));
+            console.log("I was just called");
+        }, 10000);
+        return () => clearInterval(interval);
+    }, []);
+    if (allCoinInfo === null) {
+        return <h1>Hi</h1>
+    }
+
+
+    return (
+        <div>
+            {/* <h1>{price}</h1>
+            <h1>{name}</h1>
+            <img src={btcImage} height="80"/> */}
+            {allCoinInfo.map(({current_price, name, image}) =>
+                    <div key={current_price}>
+                        <h1 key={current_price}>{current_price}</h1>
+                        <h1 key={name}>{name}</h1>
+                        <img key={image} src={image} height="40"/>
+                    </div>
+                )}
+        </div>
+    );
+}
